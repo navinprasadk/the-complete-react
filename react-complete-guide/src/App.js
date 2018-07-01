e3cf047401867a7fc2b97e6d5292ba54dbafef61
navinprasadk <navinprasad0808@gmail.com>
2018-07-01 19:52:26 +0530

    radium removed

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Radium, {StyleRoot} from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -52,10 +51,6 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover': {
-        backgroundColor:'lightgreen',
-        color: 'black'
-      }
     };
 
   let persons = null;
@@ -76,10 +71,6 @@ class App extends Component {
       </div>  
     )
     style.backgroundColor = 'red';
-    style[':hover'] = {
-      backgroundColor: 'salmon',
-      color: 'black'
-    }
   }
 
   const classes = [];
@@ -91,16 +82,14 @@ class App extends Component {
   }
 
     return (
-      <StyleRoot>
       <div className="App">
         <h1>Hi, I am a react app</h1>
         <p className = {classes.join(' ')}>This is really working</p>
         <button style = {style} onClick = {this.togglePersonHandler} >Switch Name</button>
           { persons } 
       </div>
-      </ StyleRoot>
     );
   }
 }
 
-export default Radium(App);
+export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import Radium from 'radium';
 import './Person.css';
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    }
     return(
         <div className = 'Person' style={style}>
             <p onClick = {props.click} > I' am {props.name}, I' am a {props.age} years old</p>
@@ -17,4 +11,4 @@ const person = (props) => {
     )
 };
 
-export default Radium(person);
+export default person;
